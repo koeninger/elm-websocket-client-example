ab0ce141d8a6dd46df118f08bef4b9594e1582c5
cody koeninger <cody@koeninger.org>
2016-10-23 01:03:05 -0500

    example table update via websocket

---
+++ src/Main.elm
@@ -0,0 +1,108 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.App as Html
+import WebSocket
+import Json.Decode exposing (..)
+import Dict exposing (..)
+
+
+main : Program Never
+main =
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+echoServer : String
+echoServer =
+    "ws://localhost:8080/myapp"
+
+
+
+-- MODEL
+
+
+type alias Row =
+    Dict String Int
+
+
+type alias Model =
+    { rows : List Row
+    , alert : String
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( Model [] "", Cmd.none )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = NewMessage String
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NewMessage str ->
+            case decodeString (Json.Decode.list (dict int)) str of
+                Ok rows ->
+                    ( { model | rows = rows }, Cmd.none )
+
+                Err alert ->
+                    ( { model | alert = alert }, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    WebSocket.listen echoServer NewMessage
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ viewMessage model.alert
+        , viewTable model.rows
+        ]
+
+
+viewTable : List Row -> Html Msg
+viewTable rows =
+    case List.head rows of
+        Nothing ->
+            div [] []
+
+        Just firstrow ->
+            table []
+                ((viewHeader firstrow) :: (List.map viewRow rows))
+
+
+viewHeader : Row -> Html Msg
+viewHeader firstrow =
+    tr [] (List.map (\x -> th [] [ text x ]) (Dict.keys firstrow))
+
+
+viewRow : Row -> Html Msg
+viewRow row =
+    tr [] (List.map (\x -> th [] [ text (toString x) ]) (Dict.values row))
+
+
+viewMessage : String -> Html msg
+viewMessage msg =
+    div [] [ text msg ]
